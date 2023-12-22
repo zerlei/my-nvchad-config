099a3559caf7f98f4febe5e54e58a5be4cc1a544
zhaoyouya <1445089819@qq.com>
2023-12-22 09:55:34 +0800

    change nvterm config and <C-j> <C-k> for code lint select

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -44,16 +44,42 @@ M.mason = {
 -- git support in nvimtree
 M.nvimtree = {
   git = {
-    enable =false,
+    enable = false,
   },
 
   renderer = {
     highlight_git = true,
     icons = {
       show = {
-        git =true,
+        git = true,
       },
     },
+  },
+}
+M.nvterm = {
+  terminals = {
+    shell = vim.o.shell,
+    list = {},
+    type_opts = {
+      float = {
+        relative = "editor",
+        row = 0.1,
+        col = 0.1,
+        width = 0.8,
+        height = 0.8,
+        border = "single",
+      },
+      horizontal = { location = "rightbelow", split_ratio = 0.3 },
+      vertical = { location = "rightbelow", split_ratio = 0.5 },
+    },
+  },
+  behavior = {
+    autoclose_on_quit = {
+      enabled = false,
+      confirm = true,
+    },
+    close_on_exit = true,
+    auto_insert = true,
   },
 }
 
