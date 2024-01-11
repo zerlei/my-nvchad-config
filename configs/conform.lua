f243f45c312c89c939864813e205b7e1458ae13c
zhaoyouya <1445089819@qq.com>
2024-01-11 12:56:26 +0800

    ...

--- configs/conform.lua
+++ configs/conform.lua
@@ -8,7 +8,8 @@ local options = {
 		css = { "prettier" },
     vue = { "prettier"},
 		html = { "prettier" },
-		sh = { "shfmt" }
+		sh = { "shfmt" },
+    cpp= {"clang_format"}
 	},
 
   -- adding same formatter for multiple filetypes can look too much work for some
