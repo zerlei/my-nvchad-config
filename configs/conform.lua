ea3e500e44f78d023eb15bb3db6e6ca629db3f8b
zhaoyouya <1445089819@qq.com>
2024-01-26 17:27:21 +0800

    ts formater && del null-ls.lua

--- configs/conform.lua
+++ configs/conform.lua
@@ -1,25 +1,25 @@
 local options = {
-	lsp_fallback = true,
+  lsp_fallback = true,
 
-	formatters_by_ft = {
-		lua = { "stylua" },
-
-		javascript = { "prettier" },
-		css = { "prettier" },
-    vue = { "prettier"},
-		html = { "prettier" },
-		sh = { "shfmt" },
-    cpp= {"clang_format"}
-	},
+  formatters_by_ft = {
+    lua = { "stylua" },
+    typescript = { "prettier" },
+    javascript = { "prettier" },
+    css = { "prettier" },
+    vue = { "prettier" },
+    html = { "prettier" },
+    sh = { "shfmt" },
+    cpp = { "clang_format" },
+  },
 
   -- adding same formatter for multiple filetypes can look too much work for some
   -- instead of the above code you could just use a loop! the config is just a table after all!
 
-	-- format_on_save = {
-	--   -- These options will be passed to conform.format()
-	--   timeout_ms = 500,
-	--   lsp_fallback = true,
-	-- },
+  -- format_on_save = {
+  --   -- These options will be passed to conform.format()
+  --   timeout_ms = 500,
+  --   lsp_fallback = true,
+  -- },
 }
 
 require("conform").setup(options)
